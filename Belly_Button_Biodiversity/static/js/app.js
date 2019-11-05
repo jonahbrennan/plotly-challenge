35542ab7e22fe83955885635d22ee386642c598a
jonahbrennan <jonahbrennan@gmail.com>
2019-11-05 13:59:07 -0800

    branded

--- Belly_Button_Biodiversity/static/js/app.js
+++ Belly_Button_Biodiversity/static/js/app.js
@@ -5,11 +5,57 @@ function buildMetadata(sample) {
   // Use `d3.json` to fetch the metadata for a sample
   var url = `/metadata/${sample}`;
   var metaData = url;
+  var sound0 = new Audio('../static/js/sickLittleMonkey.mp3'); 
+  var sound7 = new Audio('../static/js/superiorCreature.mp3'); 
+  var sound1 = new Audio('../static/js/filthySwine.mp3'); 
+  var sound2 = new Audio('../static/js/areYouNuts.mp3'); 
+  var sound3 = new Audio('../static/js/stinky.wav'); 
+  var sound5 = new Audio('../static/js/beautiful.wav'); 
+  var sound4 = new Audio('../static/js/boss.wav'); 
+  var sound6 = new Audio('../static/js/joy.mp3'); 
+  var soundNull = new Audio('../static/js/perfect.wav'); 
 
   // Fetch the JSON data and console log it
   // .then is Asynchronus call to request the data and keep going while waiting for data
   d3.json(url).then(function(data) {
-    // console.log(data);
+    console.log("HELLO YOU IDIOT");
+    
+      if(data.WFREQ == 0){
+          console.log("You Filthy Swine");
+          sound1.play();  
+      }
+        else if(data.WFREQ == 1){
+          console.log("YOU SICK LITTLE MONKEY");
+          sound0.play();
+      }
+        else if(data.WFREQ == 2){
+          console.log("Are You Nutz?");
+          sound2.play();
+      }
+        else if(data.WFREQ == 3){
+          console.log("STINKY");
+          sound3.play();
+      }
+        else if(data.WFREQ == 4){
+          console.log("Your the Boss");
+          sound4.play();
+      }
+        else if(data.WFREQ == 5){
+          console.log("Beautiful");
+          sound5.play();
+      }
+        else if(data.WFREQ == 6){
+          console.log("JOY");
+          sound6.play();
+      }
+        else if(data.WFREQ >= 7){
+          console.log("Hello Superior Creature");
+          sound7.play();
+      } 
+        else if(data.WFREQ == null){
+          console.log("Perfect");
+          soundNull.play();
+      }
 
   });
   
@@ -32,11 +78,16 @@ function buildMetadata(sample) {
         cell.text(key + ": " + value);
 
       });
+
       var scrubs = data.WFREQ;
+
       var data = [{
           domain: { x: [0, 1], y: [0, 1] },
           value: scrubs,
           title: { text: "Scrubs per Week" },
+
+          titlefont: {size: 45 },
+
           type: "indicator",
           mode: "gauge+number+delta",
           delta: { reference: 10 },
@@ -60,15 +111,16 @@ function buildMetadata(sample) {
                     plot_bgcolor: "rgba(0,0,0,0)",
                     paper_bgcolor: "rgba(0,0,0,0)",
                     margin: { t: 0, b: 0 }, 
-                    font: { color: "darkblue", 
-                    family: "Snowburst One" } 
+                    font: { color: "black", 
+                    family: "Ren & Stimpy",
+                    size: 50
+                    } 
                   }; // width: 600, height: 500,
 
       Plotly.newPlot('gauge', data, layout);
       
     });
     
-
 }
 
 ///// build PIE Chart HERE:
@@ -101,7 +153,9 @@ function buildCharts(sample) {
     
       var layout = {
         title: "Top Ten Organisms per Button",
-        font: { color: "rgb(255, 217, 0)", family: "Snowburst One" } ,
+        // title: { text: "Scrubs per Week" },
+        titlefont: {size: 30 },
+        font: { color: "rgb(0, 0, 0)", family: "Ren & Stimpy", size: 17 } ,
         plot_bgcolor: "rgba(0,0,0,0)",
         paper_bgcolor: "rgba(0,0,0,0)",
         // height: 600,  
@@ -128,8 +182,11 @@ function buildCharts(sample) {
     
     var layout = {
       title: 'Belly Size',
+      titlefont: {size: 50 },
+
       showlegend: false,
-      plot_bgcolor: "rgba(0,0,0,0)",
+      font: { color: "rgb(0, 0, 0)", family: "Ren & Stimpy", size: 17 } ,
+      plot_bgcolor: "rgba(100, 82, 2, 0.432)",
       paper_bgcolor: "rgba(0,0,0,0)",
       // height: 600,
       // width: 1200
